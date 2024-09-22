344d1b262659a4ba5fec37088ec11e363d84e9f3
Matheus Pacheco Cruz <pachecx35@gmail.com>
2024-09-21 22:08:12 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [tarefa, setTarefa] = useState<string>('')
+
+  const Salvar = (e) => {
+    e.preventDefault()
+  }
+
+  console.log(tarefa)
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="bg-slate-200 h-screen p-8">
+      <div className="">
+        <div>
+          <h1 className="text-3xl font-bold ">Lista de tarefas</h1>
+
+          <form className="mt-4">
+            <input
+              className="p-1 rounded"
+              onChange={(e) => setTarefa(e.target.value)}
+              type="text"
+              placeholder="Adicione uma nova tarefa"
+            />
+            <button type="submit" className="bg-blue-500 border rounded p-1 text-white">
+              Adicionar
+            </button>
+          </form>
+        </div>
+
+        {tarefa.length == 0 ? (
+           <div className=" bg-white mt-4 p-2 rounded justify-between w-1/3 items-center">
+           <p>Adicione uma tarefa</p>
+         </div>
+        ):(
+          <div className="flex bg-white mt-4 p-2 rounded justify-between w-1/3 items-center">
+          <button>
+            <p className="">Tarefa 1</p>
+          </button>
+          <button className="bg-green-500 p-1 rounded text-white">
+            Remover
+          </button>
+        </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
